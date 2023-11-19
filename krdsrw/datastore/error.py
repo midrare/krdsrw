38a937cb6f1e549397fe47e36a01e1591d36acf5
midrare <midrare9@gmail.com>
2023-11-18 16:32:56 -0800

    refactor: error displays data in hex

--- krdsrw/datastore/error.py
+++ krdsrw/datastore/error.py
@@ -28,14 +28,14 @@ class UnexpectedBytesError(Exception):
             self, pos: int, expected: int | bytes, actual: None | int | bytes):
         s = f"@{pos} expected "
         s += f"0x{expected:02x} " if isinstance(
-            expected, int) else f"{str(expected)} "
+            expected, int) else f"{self._to_hex(expected)} "
 
         if actual is None:
             s += "but not found."
         else:
             s += f"but got "
             s += f"0x{actual:02x}" if isinstance(
-                actual, int) else f"{str(actual)}"
+                actual, int) else f"{self._to_hex(actual)}"
             s += "."
 
         super().__init__(s)
@@ -43,6 +43,15 @@ class UnexpectedBytesError(Exception):
         self._pos: int = pos
         self._expected: int | bytes = expected
         self._actual: None | int | bytes = actual
+
+    @staticmethod
+    def _to_hex(data: bytes, prefix: bool = True) -> str:
+        s = ''
+        if prefix:
+            s += '0x'
+        for b in data:
+            s += f"{b:02x}"
+        return s
 
     @property
     def pos(self) -> int:
