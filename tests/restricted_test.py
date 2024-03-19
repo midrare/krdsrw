0bea924842152cf1658d353101553d039dcc6536
midrare <midrare9@gmail.com>
2024-03-18 17:01:30 -0700

    test: each hook separately

--- tests/restricted_test.py
+++ tests/restricted_test.py
@@ -8,62 +8,67 @@ from krdsrw.restricted import RestrictedDict
 from krdsrw.restricted import RestrictedList
 
 
-
-class _List(RestrictedList[int]):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.is_modified: bool = False
-
-    @typing.override
-    def _pre_write_filter(self, value: int) -> bool:
-        return isinstance(value, int) and value % 2 == 0
-
-    @typing.override
-    def _pre_write_transform(self, value: int) -> int:
-        return value**2
-
-    @typing.override
-    def _post_write_hook(self):
-        self.is_modified = True
-
-
 class TestRestrictedList:
     def test_instantiate(self):
-        o = _List()  # no error
+        class CC(RestrictedList[int]):
+            pass
+
+        o = CC()
         assert o is not None
 
-    def test_init(self):
-        _List([ 2, 4, 6, 8 ])  # no error
-        with pytest.raises(ValueError):
-            _List([ 1, 2, 3, 4 ])
+    def test_write_filter(self):
+        class CC(RestrictedList[int]):
+            @typing.override
+            def _pre_write_filter(self, value: int) -> bool:
+                return isinstance(value, int) and value % 2 == 0
 
-    def test_append(self):
-        o = _List()
+        o = CC([ 2, 4, 6, 8 ])
+        o[1] = 12  # no error
+        with pytest.raises(ValueError):
+            o[1] = 7
+
+        o = CC()
         o.append(8)  # no error
         with pytest.raises(ValueError):
             o.append('ABC')
         with pytest.raises(ValueError):
             o.append(9)
 
-    def test_read_index(self):
-        o = _List([ 2, 4, 6, 8 ])
-        assert o[1] == 16  # no error
-        with pytest.raises(IndexError):
-            o[99]
+    def test_write_transform(self):
+        class CC(RestrictedList[int]):
+            @typing.override
+            def _pre_write_transform(self, value: int) -> int:
+                return value**2
 
-    def test_write_index(self):
-        o = _List([ 2, 4, 6, 8 ])
+        o = CC([ 2, 4, 6, 8 ])
         o[1] = 12  # no error
         with pytest.raises(ValueError):
             o[1] = 7
 
-    def test_modified(self):
-        o = _List()
+
+        o = CC()
+        o.append(8)  # no error
+        with pytest.raises(ValueError):
+            o.append('ABC')
+        with pytest.raises(ValueError):
+            o.append(9)
+
+    def test_post_write_hook(self):
+        class CC(RestrictedList[int]):
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                self.is_modified: bool = False
+
+            @typing.override
+            def _post_write_hook(self):
+                self.is_modified = True
+
+        o = CC()
         assert not o.is_modified
         o.append(2)
         assert o.is_modified
 
-        o = _List([ 2, 4, 6, 8 ])
+        o = CC([ 2, 4, 6, 8 ])
         assert not o.is_modified
         o[1] = 24
         assert o.is_modified
