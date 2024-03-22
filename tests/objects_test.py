509295acd2f451b1b1c9e5082fd8998c8ea63f3d
midrare <midrare9@gmail.com>
2024-03-22 15:08:46 -0700

    test: merge into single module

--- tests/objects_test.py
+++ tests/objects_test.py
@@ -1,3 +1,6 @@
+import typing
+import pathlib
+
 import pytest
 
 from krdsrw.cursor import Cursor
@@ -12,6 +15,7 @@ from krdsrw.specs import Spec
 from krdsrw.specs import Index
 from krdsrw.specs import Field
 from krdsrw.objects import Array
+from krdsrw.objects import DataStore
 from krdsrw.objects import DynamicMap
 from krdsrw.objects import Object
 from krdsrw.objects import Record
@@ -19,6 +23,15 @@ from krdsrw.objects import peek_object_schema
 from krdsrw.objects import peek_object_type
 from krdsrw.objects import read_object
 from krdsrw.objects import write_object
+
+TEMPEST_EPUB: typing.Final[
+    pathlib.Path] = pathlib.Path(__file__).parent / "The Tempest.epub"
+TEMPEST_KFX: typing.Final[
+    pathlib.Path] = pathlib.Path(__file__).parent / "The Tempest.kfx"
+TEMPEST_YJF: typing.Final[
+    pathlib.Path] = pathlib.Path(__file__).parent / "The Tempest.yjf"
+TEMPEST_YJR: typing.Final[
+    pathlib.Path] = pathlib.Path(__file__).parent / "The Tempest.yjr"
 
 
 def test_peek_object_schema():
@@ -192,3 +205,46 @@ def test_dynamic_map_put_key():
     o['a'] = Int(0x0a)
     with pytest.raises(ValueError):
         o['a'] = 0x0a  # type: ignore
+
+
+class TestDataStore:
+    def test_init(self):
+        root = DataStore()
+
+    def test_read(self):
+        csr = Cursor(TEMPEST_YJR.read_bytes())
+        root = DataStore()
+        root.read(csr)
+
+    def test_read_write(self):
+        orig_data = TEMPEST_YJR.read_bytes()
+
+        csr = Cursor(orig_data)
+        root = DataStore()
+        root.read(csr)
+
+        csr = Cursor()
+        root.write(csr)
+
+        assert csr.dump() == orig_data
+
+    def test_annotation_cache_object_len(self):
+        csr = Cursor(TEMPEST_YJR.read_bytes())
+        root = DataStore()
+        root.read(csr)
+        assert len(root["annotation.cache.object"]) == 3
+
+    def test_bookmark(self):
+        csr = Cursor(TEMPEST_YJR.read_bytes())
+        root = DataStore()
+        root.read(csr)
+
+        o = root["annotation.cache.object"]["bookmarks"][0]
+        assert o["start_pos"].chunk_eid == 5525
+        assert o["start_pos"].chunk_pos == 0
+        assert o["start_pos"].char_pos == 76032
+        assert o["end_pos"].chunk_eid == 5525
+        assert o["end_pos"].chunk_pos == 0
+        assert o["end_pos"].char_pos == 76032
+        assert o["creation_time"].epoch_ms == 1701332599082
+        assert o["last_modification_time"].epoch_ms == 1701332599082
