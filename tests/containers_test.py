3c8f35b4ba0c135de73a07439ba1ab809034f17b
midrare <midrare9@gmail.com>
2024-03-29 18:30:30 -0700

    test: better name for custom subclass

--- tests/containers_test.py
+++ tests/containers_test.py
@@ -10,24 +10,24 @@ from krdsrw.containers import ListBase
 
 class TestListBase:
     def test_instantiate(self):
-        class CC(ListBase[int]):
+        class CustomClass(ListBase[int]):
             pass
 
-        o = CC()
+        o = CustomClass()
         assert o is not None
 
     def test_write_filter(self):
-        class CC(ListBase[int]):
+        class CustomClass(ListBase[int]):
             @typing.override
             def _is_allowed(self, value: int) -> bool:
                 return isinstance(value, int) and value % 2 == 0
 
-        o = CC([ 2, 4, 6, 8 ])
+        o = CustomClass([ 2, 4, 6, 8 ])
         o[1] = 12  # no error
         with pytest.raises(ValueError):
             o[1] = 7
 
-        o = CC()
+        o = CustomClass()
         o.append(8)  # no error
         with pytest.raises(ValueError):
             o.append('ABC')
@@ -35,32 +35,32 @@ class TestListBase:
             o.append(9)
 
     def test_write_transform(self):
-        class CC(ListBase[int]):
+        class CustomClass(ListBase[int]):
             @typing.override
             def _transform(self, value: int) -> int:
                 return value**2
 
-        o = CC()
+        o = CustomClass()
         o.append(8)
         assert o == [64]
 
-        o = CC([ 2, 4, 6, 8 ])
+        o = CustomClass([ 2, 4, 6, 8 ])
         assert o == [ 4, 16, 36, 64 ]
 
-        o = CC([ 2, 4, 6, 8 ])
+        o = CustomClass([ 2, 4, 6, 8 ])
         o[1] = 12  # no error
         assert o == [ 4, 144, 36, 64 ]
 
     def test_post_write_hook(self):
-        class CC(ListBase[int]):
+        class CustomClass(ListBase[int]):
             pass
 
-        o = CC()
+        o = CustomClass()
         assert not o.is_modified
         o.append(2)
         assert o.is_modified
 
-        o = CC([ 2, 4, 6, 8 ])
+        o = CustomClass([ 2, 4, 6, 8 ])
         assert not o.is_modified
         o[1] = 24
         assert o.is_modified
@@ -85,7 +85,7 @@ class TestDictBase:
         assert o == { 'a': 'A', 'b': 'B', 'c': 'C'}
 
     def test_or_operator(self):
-        class CC(DictBase[str, int]):
+        class CustomClass(DictBase[str, int]):
             @typing.override
             def _is_key_value_writable(
                 self,
@@ -94,28 +94,28 @@ class TestDictBase:
             ) -> bool:
                 return isinstance(key, str) and isinstance(value, int)
 
-        o = CC({ 'a': 123 }) | { 'b': 456 }  # no error
+        o = CustomClass({ 'a': 123 }) | { 'b': 456 }  # no error
         with pytest.raises(ValueError):
-            o = CC({ 'a': 123 }) | { 'b': b'ZZZ'}
+            o = CustomClass({ 'a': 123 }) | { 'b': b'ZZZ'}
 
-        o = { 'float': 1.23 } | CC({ 'int': 456 })
-        o = { 'bytes': b'ZZZ'} | CC({ 'int': 456 })
+        o = { 'float': 1.23 } | CustomClass({ 'int': 456 })
+        o = { 'bytes': b'ZZZ'} | CustomClass({ 'int': 456 })
 
-        o = CC({ 'a': 1 })
+        o = CustomClass({ 'a': 1 })
         o |= { 'b': 2 }
 
         with pytest.raises(ValueError):
-            o = CC({ 'a': 123 })
+            o = CustomClass({ 'a': 123 })
             o |= { 'b': b'ZZZ'}
 
         o = { 'float': 1.23 }
-        o |= CC({ 'int': 456 })
+        o |= CustomClass({ 'int': 456 })
 
         o = { 'bytes': b'ZZZ'}
-        o |= CC({ 'int': 456 })
+        o |= CustomClass({ 'int': 456 })
 
     def test_write_filter(self):
-        class CC(DictBase[str, int]):
+        class CustomClass(DictBase[str, int]):
             @typing.override
             def _is_key_value_writable(
                 self,
@@ -124,39 +124,39 @@ class TestDictBase:
             ) -> bool:
                 return len(key) <= 1 and isinstance(value, int)
 
-        CC({ 'a': 1, 'b': 2, 'c': 3 })
+        CustomClass({ 'a': 1, 'b': 2, 'c': 3 })
 
         with pytest.raises(ValueError):
-            CC({ 'a': b'AAA', 'b': b'BBB', 'c': b'CCC'})
+            CustomClass({ 'a': b'AAA', 'b': b'BBB', 'c': b'CustomClassC'})
 
         with pytest.raises(ValueError):
-            CC({ 'aa': 1, 'bb': 2, 'cc': 3 })
+            CustomClass({ 'aa': 1, 'bb': 2, 'cc': 3 })
 
-        o = CC()
+        o = CustomClass()
         o.setdefault('a', 9)
         assert o == { 'a': 9 }
 
         with pytest.raises(ValueError):
-            o = CC()
+            o = CustomClass()
             o.setdefault('aa', 4)
 
         with pytest.raises(ValueError):
-            o = CC()
+            o = CustomClass()
             o.setdefault('a', b'ZZZ')  # pyright: ignore  # type: ignore
 
-        o = CC()
+        o = CustomClass()
         o.update({ 'a': 123, 'b': 456 })
         assert o == { 'a': 123, 'b': 456 }
 
         with pytest.raises(ValueError):
-            o = CC()
+            o = CustomClass()
             o.update({
                 'a': 12,
                 'b': 4.5,  # pyright: ignore  # type: ignore
             })
 
     def test_write_transform(self):
-        class CC(DictBase[str, int]):
+        class CustomClass(DictBase[str, int]):
             @typing.override
             def _transform_key_value(
                 self,
@@ -165,48 +165,48 @@ class TestDictBase:
             ) -> tuple[str, int]:
                 return key.capitalize(), value * 2
 
-        o = CC()
+        o = CustomClass()
         assert o == {}
 
-        o = CC({})
+        o = CustomClass({})
         assert o == {}
 
-        o = CC({ 'a': 1 })
+        o = CustomClass({ 'a': 1 })
         assert o == { 'A': 2 }
 
-        o = CC({ 'a': 1, 'b': 2, 'c': 3 })
+        o = CustomClass({ 'a': 1, 'b': 2, 'c': 3 })
         assert o == { 'A': 2, 'B': 4, 'C': 6 }
 
     def test_read_filter(self):
-        class CC(DictBase[typing.Any, typing.Any]):
+        class CustomClass(DictBase[typing.Any, typing.Any]):
             @typing.override
             def _is_key_readable(self, key: typing.Any) -> bool:
                 return isinstance(key, str) and len(key) <= 1
 
         with pytest.raises(KeyError):
-            o = CC()
+            o = CustomClass()
             o['a']
 
-        o = CC()
+        o = CustomClass()
         assert o.get('a') is None
 
-        o = CC()
+        o = CustomClass()
         assert o.get('a', 0) == 0
 
         with pytest.raises(KeyError):
-            o = CC()
+            o = CustomClass()
             o['aa']
 
         with pytest.raises(KeyError):
-            o = CC()
+            o = CustomClass()
             o.get('aa')
 
         with pytest.raises(KeyError):
-            o = CC()
+            o = CustomClass()
             o.get('aa', 0)
 
     def test_read_transform(self):
-        class CC(DictBase[typing.Any, typing.Any]):
+        class CustomClass(DictBase[typing.Any, typing.Any]):
             @typing.override
             def _transform_key(self, key: typing.Any) -> typing.Any:
                 if isinstance(key, str):
@@ -214,71 +214,71 @@ class TestDictBase:
                 return key
 
         with pytest.raises(KeyError):
-            o = CC({ 'a': 1, 'b': 2, 'c': 3 })
+            o = CustomClass({ 'a': 1, 'b': 2, 'c': 3 })
             o['z']
 
-        o = CC({ 'a': 1, 'b': 2, 'c': 3 })
+        o = CustomClass({ 'a': 1, 'b': 2, 'c': 3 })
         assert o.get('A') == 1
 
-        o = CC({ 'a': 1, 'b': 2, 'c': 3 })
+        o = CustomClass({ 'a': 1, 'b': 2, 'c': 3 })
         assert o.get('B', 0) == 2
 
         with pytest.raises(KeyError):
-            o = CC()
+            o = CustomClass()
             o['aa']
 
-        o = CC()
+        o = CustomClass()
         assert o.get('aa') is None
 
-        o = CC()
+        o = CustomClass()
         assert o.get('aa', 0) == 0
 
     def test_del_filter(self):
-        class CC(DictBase[typing.Any, typing.Any]):
+        class CustomClass(DictBase[typing.Any, typing.Any]):
             @typing.override
             def _is_key_deletable(self, key: typing.Any) -> typing.Any:
                 return not isinstance(key, str) or not key.startswith('req_')
 
         with pytest.raises(KeyError):
-            o = CC()
+            o = CustomClass()
             del o['a']
 
         with pytest.raises(KeyError):
-            o = CC()
+            o = CustomClass()
             del o['req_a']
 
         with pytest.raises(KeyError):
-            o = CC({})
+            o = CustomClass({})
             del o['a']
 
         with pytest.raises(KeyError):
-            o = CC({})
+            o = CustomClass({})
             del o['req_a']
 
-        o = CC({ 'a': 1 })
+        o = CustomClass({ 'a': 1 })
         del o['a']
 
         with pytest.raises(KeyError):
-            o = CC({ 'req_a': 1 })
+            o = CustomClass({ 'req_a': 1 })
             del o['req_a']
 
         with pytest.raises(KeyError):
-            o = CC({ 'a': 1 })
+            o = CustomClass({ 'a': 1 })
             del o['req_a']
 
-        o = CC({ 'a': 1, 'b': 2, 'c': 3 })
+        o = CustomClass({ 'a': 1, 'b': 2, 'c': 3 })
         del o['a']
 
         with pytest.raises(KeyError):
-            o = CC({ 'a': 1, 'b': 2, 'c': 3 })
+            o = CustomClass({ 'a': 1, 'b': 2, 'c': 3 })
             del o['req_a']
 
         with pytest.raises(KeyError):
-            o = CC({ 'req_a': 1, 'req_b': 2, 'req_c': 3 })
+            o = CustomClass({ 'req_a': 1, 'req_b': 2, 'req_c': 3 })
             del o['req_a']
 
     def test_del_transform(self):
-        class CC(DictBase[typing.Any, typing.Any]):
+        class CustomClass(DictBase[typing.Any, typing.Any]):
             @typing.override
             def _transform_key(self, key: typing.Any) -> typing.Any:
                 if isinstance(key, str):
@@ -286,120 +286,120 @@ class TestDictBase:
                 return key
 
         with pytest.raises(KeyError):
-            o = CC()
+            o = CustomClass()
             del o['a']
 
         with pytest.raises(KeyError):
-            o = CC()
+            o = CustomClass()
             del o['A']
 
         with pytest.raises(KeyError):
-            o = CC({})
+            o = CustomClass({})
             del o['a']
 
         with pytest.raises(KeyError):
-            o = CC({})
+            o = CustomClass({})
             del o['A']
 
-        o = CC({ 'a': 1 })
+        o = CustomClass({ 'a': 1 })
         del o['a']
         assert o == {}
 
-        o = CC({ 'a': 1 })
+        o = CustomClass({ 'a': 1 })
         del o['A']
         assert o == {}
 
-        o = CC({ 'a': 1 })
+        o = CustomClass({ 'a': 1 })
         del o['A']
         assert o == {}
 
-        o = CC({ 'a': 1, 'b': 2, 'c': 3 })
+        o = CustomClass({ 'a': 1, 'b': 2, 'c': 3 })
         del o['a']
         assert o == { 'b': 2, 'c': 3 }
 
-        o = CC({ 'a': 1, 'b': 2, 'c': 3 })
+        o = CustomClass({ 'a': 1, 'b': 2, 'c': 3 })
         del o['A']
         assert o == { 'b': 2, 'c': 3 }
 
     def test_modified_hook(self):
-        class CC(DictBase[typing.Any, typing.Any]):
+        class CustomClass(DictBase[typing.Any, typing.Any]):
             pass
 
-        o = CC({ 'a': 1, 'b': 2, 'c': 3 })
+        o = CustomClass({ 'a': 1, 'b': 2, 'c': 3 })
         assert not o.is_modified
 
-        o = CC()
+        o = CustomClass()
         o.setdefault('a', 9)
         assert o.is_modified
 
-        o = CC({ 'a': 1, 'b': 2, 'c': 3 })
+        o = CustomClass({ 'a': 1, 'b': 2, 'c': 3 })
         o.setdefault('a', 9)
         assert not o.is_modified
 
-        o = CC()
+        o = CustomClass()
         o.update({ 'a': 123, 'b': 456 })
         assert o.is_modified
 
     def test_chain_single_read(self):
-        class CC(DictBase[typing.Any, typing.Any]):
+        class CustomClass(DictBase[typing.Any, typing.Any]):
             def _make_standin(self, key: typing.Any) -> None | typing.Any:
                 return self.__class__()
 
-        o = CC()
+        o = CustomClass()
         o['a0']
         assert o == {}
 
     def test_chain_single_write(self):
-        class CC(DictBase[typing.Any, typing.Any]):
+        class CustomClass(DictBase[typing.Any, typing.Any]):
             def _make_standin(self, key: typing.Any) -> None | typing.Any:
                 return self.__class__()
 
-        o = CC()
+        o = CustomClass()
         o['a0'] = 'hello'
         assert o == { 'a0': 'hello'}
 
     def test_chain_nested_lvl2_read(self):
-        class CC(DictBase[typing.Any, typing.Any]):
+        class CustomClass(DictBase[typing.Any, typing.Any]):
             def _make_standin(self, key: typing.Any) -> None | typing.Any:
                 return self.__class__()
 
-        o = CC()
+        o = CustomClass()
         o['a0']['b0']
         assert o == {}
 
     def test_chain_nested_lvl2_write(self):
-        class CC(DictBase[typing.Any, typing.Any]):
+        class CustomClass(DictBase[typing.Any, typing.Any]):
             def _make_standin(self, key: typing.Any) -> None | typing.Any:
                 return self.__class__()
 
-        o = CC()
+        o = CustomClass()
         o['a0']['b0'] = 'hello'
         assert o == { 'a0': { 'b0': 'hello'} }
 
     def test_chain_nested_lvl3_read(self):
-        class CC(DictBase[typing.Any, typing.Any]):
+        class CustomClass(DictBase[typing.Any, typing.Any]):
             def _make_standin(self, key: typing.Any) -> None | typing.Any:
                 return self.__class__()
 
-        o = CC()
+        o = CustomClass()
         o['a0']['b0']['c0']
         assert o == {}
 
     def test_chain_nested_lvl3_write(self):
-        class CC(DictBase[typing.Any, typing.Any]):
+        class CustomClass(DictBase[typing.Any, typing.Any]):
             def _make_standin(self, key: typing.Any) -> None | typing.Any:
                 return self.__class__()
 
-        o = CC()
+        o = CustomClass()
         o['a0']['b0']['c0'] = 'hello'
         assert o == { 'a0': { 'b0': { 'c0': 'hello'} } }
 
     def test_chain_nested_lvl4_write(self):
-        class CC(DictBase[typing.Any, typing.Any]):
+        class CustomClass(DictBase[typing.Any, typing.Any]):
             def _make_standin(self, key: typing.Any) -> None | typing.Any:
                 return self.__class__()
 
-        o = CC()
+        o = CustomClass()
         o['a0']['b0']['c0'] = 'hello'
         o['a0']['b0']['c1'] = 'world'
         o['a0']['b1'] = 'lorem'
